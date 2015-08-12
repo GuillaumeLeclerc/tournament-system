77bd981ada2f1fd47c6a4306b603352d61a85768
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2015-08-12 14:38:47 +0200

    add notification api

---
+++ api/models/Notification.js
@@ -0,0 +1,35 @@
+/**
+* Notification.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+	  id : {
+		  type : "integer",
+		  primaryKey : true,
+		  unique : true,
+		  autoIncrement : true
+	  },
+
+	  title : {
+		  type : "string",
+		  required : true
+	  }, 
+
+	  message : {
+		  type : "string",
+		  required : true,
+	  }, 
+
+	  target : {
+		  model : "user",
+		  required : true
+	  }
+  }
+};
+
