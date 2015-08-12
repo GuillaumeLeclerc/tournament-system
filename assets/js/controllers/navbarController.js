9d56ea37a9c14a3957a5aa8aba44b18553bc498b
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2015-08-12 09:24:40 +0200

    display the version number

--- assets/js/controllers/navbarController.js
+++ assets/js/controllers/navbarController.js
@@ -1,4 +1,5 @@
 var app = angular.module("tournament-system");
-app.controller("navbarController", ["$scope", "ts-title", function($scope, title) {
+app.controller("navbarController", ["$scope", "ts-title", "ts-version", function($scope, title, version) {
 	$scope.projectName = title;
+	$scope.version = version;
 }]);
