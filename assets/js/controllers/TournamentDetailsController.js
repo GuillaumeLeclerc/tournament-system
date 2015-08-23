564a1d98bfab41888d1d194c6fd689bb73419cd6
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2015-08-24 00:15:09 +0200

    add progress bar for nb teams in a tournament

--- assets/js/controllers/TournamentDetailsController.js
+++ assets/js/controllers/TournamentDetailsController.js
@@ -2,4 +2,11 @@ var app = angular.module("tournament-system");
 
 app.controller("TournamentDetailsController", ["$scope", "Tournament", "$routeParams", function($scope, Tournament, $routeParams) {
 	$scope.tournament = Tournament.get({id : $routeParams.id});
+	$scope.getFillPercentage = function () {
+		if ($scope.tournament.teams) {
+			return $scope.tournament.teams.length / $scope.tournament.nbMaxTeam * 100;
+		} else {
+			return 0;
+		}
+	};
 }]);
